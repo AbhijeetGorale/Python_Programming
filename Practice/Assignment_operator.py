c93cb04c0c8c4dc43ad01cc69310920765288a09
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-05 21:03:18 +0530

    Add files via upload

---
+++ Practice/Assignment_operator.py
@@ -0,0 +1,22 @@
+a  = 5 
+b = 5
+c = a +b
+print(c)    #10
+
+z = 50
+x = 5
+y =z / x
+print(y)  #10.0
+
+z = 50
+x = 5
+y =z // x
+print(y) #10
+
+a = 10
+a += 3
+print(a)  #13
+
+a = 10
+a -= 3
+print(a)  #7
