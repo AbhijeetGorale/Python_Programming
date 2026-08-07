383a4a4273e5ecc821b1117c0764b5a1f13ba08d
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-07 11:56:42 +0530

    Add files via upload

---
+++ Practice/Escape_sequence.py
@@ -0,0 +1,18 @@
+# a = " abhijeet"  #-> only prints abhijeet 
+# "Gorale"        # not allowed
+# "Morshi"        #not allowed
+
+# print(a)
+
+a = "abhijeet \nGorale \nMorshi"
+print(a)    # output ->abhijeet
+            #          gorale
+            #          morshi
+
+#(\n -> for new line )
+
+b = "abhijeet \"gorale\""  #output->abhijeet "gorale"
+print(b)
+
+
+
