d76689de4e60ae77f5cc1892b48fb6d57f46ffae
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-07 11:55:57 +0530

    Add files via upload

---
+++ Practice/Comparision_oprator.py
@@ -0,0 +1,13 @@
+a = int(input("enter the first value :"))
+b = int(input("enter the second value :"))
+
+c = a > b
+
+if(a > b):
+    print("a is greater than b")
+
+else:
+    print("b is greater than a")
+
+
+
